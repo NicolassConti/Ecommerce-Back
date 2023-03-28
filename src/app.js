ca431b15b16f2e81531ccdc1cd2f8031500bb309
nicolassconti <continicolas6@gmail.com>
2023-03-28 20:12:07 -0300

    routes created

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 const express = require("express");
-const app = express();
 const db = require("./utils/database")
+const morgan = require("morgan");
+const cors = require("cors");
 const initModels = require("./models/initModels");
+const userRoutes = require("./routes/user.routes");
 
 const PORT = 6000;
 
 initModels();
+
+const app = express();
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cors());
 
 db.authenticate()
     .then(() => {
@@ -16,7 +23,7 @@ db.authenticate()
     })
 
 
-db.sync()
+db.sync({alter:true})
     .then(() => {
         console.log("Database Syncronized");
     })
@@ -27,6 +34,8 @@ db.sync()
 app.get("/", (req, res) => {
     res.send("Bienvenido a mi Ecommerce")
 })
+
+app.use(userRoutes);
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el ${PORT}`);
---
+++ src/controllers/user.controllers.js
@@ -0,0 +1,17 @@
+const userServices = require("../services/user.services");
+
+const createUser = async(req, res, next) =>{
+    try {
+        const newUser = req.body
+        const result = await userServices.create(newUser)
+        res.status(200).json(result)
+    } catch (error) {
+        next(error)
+    }
+}
+
+
+module.exports = {
+    createUser
+};
+
